9806ec42414d2b0c31928143a6fbfb043f15acae
Caio Arruda Rodrigues Lemes <caioarrudar.l2003@gmail.com>
2024-11-26 20:58:41 -0300

    melhorias validacao cpf usando dependencia

--- src/main/java/br/edu/ifsp/spo/bike_integration/util/validate/CpfValidate.java
+++ src/main/java/br/edu/ifsp/spo/bike_integration/util/validate/CpfValidate.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Component;
 
+import br.com.caelum.stella.validation.CPFValidator;
+
 @Component
 public class CpfValidate {
 
@@ -32,38 +34,7 @@ public class CpfValidate {
     }
 
     private boolean isCpfValid(String cpf) {
-        int[] numbers = new int[11];
-        for (int i = 0; i < 11; i++) {
-            numbers[i] = Integer.parseInt(cpf.substring(i, i + 1));
-        }
-        int sum = 0;
-        for (int i = 0; i < 9; i++) {
-            sum += numbers[i] * (10 - i);
-        }
-        int remainder = sum % 11;
-        if (remainder == 1 || remainder == 0) {
-            if (numbers[9] != 0) {
-                return false;
-            }
-        } else {
-            if (numbers[9] != 11 - remainder) {
-                return false;
-            }
-        }
-        sum = 0;
-        for (int i = 0; i < 10; i++) {
-            sum += numbers[i] * (11 - i);
-        }
-        remainder = sum % 11;
-        if (remainder == 1 || remainder == 0) {
-            if (numbers[10] != 0) {
-                return false;
-            }
-        } else {
-            if (numbers[10] != 11 - remainder) {
-                return false;
-            }
-        }
-        return true;
+    	CPFValidator cpfValidator = new CPFValidator();
+    	return cpfValidator.invalidMessagesFor(cpf).isEmpty();
     }
 }
